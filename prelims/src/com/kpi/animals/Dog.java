d19f021a710ccd336acf2cbbac0ad6cbccf81b5d
srivalli-bandaru <114054086+srivalli-bandaru@users.noreply.github.com>
2022-09-23 17:11:47 +0530

    push 1

--- prelims/src/com/kpi/animals/Dog.java
+++ prelims/src/com/kpi/animals/Dog.java
@@ -1,5 +1,10 @@
 package com.kpi.animals;
 
-public class Dog {
+
+//public class Dog extens animal
+//dog is Omnivours: is a relationship-inheritance
+//child=dog,parent=Omnivours
+//extends=bind child to the parent
+public class Dog extends Omnivours {
 
 }
