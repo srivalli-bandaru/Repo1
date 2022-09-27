fb47a463a7a5ac10398a8cee462f5f8b7de5cb7c
srivalli-bandaru <114054086+srivalli-bandaru@users.noreply.github.com>
2022-09-27 12:43:41 +0530

    commit9

--- ObjectOriented1/src/Practice/Human.java
+++ ObjectOriented1/src/Practice/Human.java
@@ -1,24 +1,23 @@
 package Practice;
 
-public class Dora implements Skeleton{
+public class Human implements Skeleton {
 
 	@Override
 	public void brain() {
 		// TODO Auto-generated method stub
-		System.out.println("I am a cartoon");
+		System.out.println("I am Clever");
 	}
 
 	@Override
 	public void flesh() {
 		// TODO Auto-generated method stub
-		System.out.println("I am vegetarian");
+		System.out.println("I will eat all ");
 	}
 
 	@Override
 	public void color() {
 		// TODO Auto-generated method stub
-		System.out.println("I am Blue");
+		System.out.println("I am normal");
 	}
-	
 
 }
