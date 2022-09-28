8d06a9076a1482e96984fe5639c1b5a42b5e2108
srivalli-bandaru <114054086+srivalli-bandaru@users.noreply.github.com>
2022-09-28 11:47:46 +0530

    commit for MO

---
+++ ObjectOriented1/src/Polymorphism/MethodOverload.java
@@ -0,0 +1,23 @@
+package Polymorphism;
+
+public class MethodOverload {
+	
+	public MethodOverload() {
+		
+	}
+	
+	public int MethodOverload() {
+	return 1;	
+		
+	}
+	public MethodOverload(int a,int b) {
+		
+	}
+static int abc() {
+	return 1;
+	
+}
+static String abc(int a , int b) {
+	return "1";
+}
+}
---
+++ ObjectOriented1/src/Polymorphism/TestMethodOverloading.java
@@ -0,0 +1,12 @@
+package Polymorphism;
+
+public class TestMethodOverloading extends MethodOverload {
+	public static void main(String a[]) {
+		
+		MethodOverload mo = new MethodOverload();
+		mo.abc();
+		mo.abc(0,0);
+		
+	}
+
+}
