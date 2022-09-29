7af40a5a874d816039bc900c5c002d644e0a9d46
srivalli-bandaru <114054086+srivalli-bandaru@users.noreply.github.com>
2022-09-29 10:51:42 +0530

    commit for collection

---
+++ OOps3/src/Practice1/TestCollection.java
@@ -0,0 +1,30 @@
+package Practice1;
+
+import java.util.ArrayList;
+
+/**
+ * @author SrivalliBandaru
+ *
+ */
+public class TestCollection {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> list = new ArrayList<String>();
+		ArrayList list2 = new ArrayList();
+
+		list.add("a");
+		list.add("b");
+		//list.add(11);
+		list2.add(11);
+		//System.out.println(list.get(1));
+		//System.out.println(list);
+		//System.out.println(list2);
+		for(int i=0; i<list.size(); i++)
+		{
+			if(list.get(i)=="z")//i=0,1....
+			System.out.println(i);
+	}
+}
+}
+
